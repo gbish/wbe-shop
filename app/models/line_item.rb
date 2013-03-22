066cba0493c8e9edd223709c0dd57f9ae1854167
Gareth Bishop <hebgb@blueyonder.co.uk>
2013-03-22 04:20:08 +0000

    Fixed a issue in the line_item model breaking the hidden cart view

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -4,7 +4,9 @@ class LineItem < ActiveRecord::Base
   
   # establishes an association with the listed models
   # this is the ONE side in a ONE to many association
-  belongs_to :order, :product, :cart
+  belongs_to :order
+  belongs_to :product
+  belongs_to :cart
   
   # calculates the total price of a line item row
   def total_price
